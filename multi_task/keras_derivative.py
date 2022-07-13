990921002a002e3c4eea74bd39d15b1527743ba4
caoxiaoqiang <checao@tencent.com>
2022-07-13 16:46:21 +0800

    tf.function notation

--- multi_task/keras_derivative.py
+++ multi_task/keras_derivative.py
@@ -199,8 +199,88 @@ def tape_sgd_test03b():
     print(x, f(x))
 
 
+def func00():
+    a = tf.constant([[10, 10], [11., 1.]])
+    x = tf.constant([[1., 0.], [0., 1.]])
+    b = tf.Variable(12.)
+    y = tf.matmul(a, x) + b
+    print("PRINT: ", y)
+    tf.print("TF-PRINT: ", y)
+    return y
+
+
+@tf.function
+def func01():
+    """
+    ValueError: tf.function-decorated function tried to create variables on non-first call.
+    因为 tf.function可能会对一段Python函数进行多次执行来构图，在多次执行的过程中，同样的Variable被创建了多次，产生错误．
+    这其实也是一个很容易混乱的概念，在eager mode下一个Variable是一个Python object，所以会在执行范围外被销毁．但是在 tf.function的装饰下，Variable变成了 tf.Variable，是在Graph中持续存在的．
+    把一个在eager mode下正常执行的函数转换到Tensorflow图形式，需要一边思考着计算图一边构建程序．
+
+    因为tf.function可能会对一段python代码进行多次执行来进行graph的构建，在多次的执行过程中，Variable被创建了多次，
+    而tensorflow 2.x文档中明确指出*State (like tf.Variable objects) are only created the first time the function f is called.*。所以就报错了。
+    因此我们在构建被tf.function修饰的函数时，一定要记得保证每一个tf.Variable变量只被创建一次，否则就有可能报错。
+
+    以下两种情况不适合使用 tf.function 进行修饰：
+    1.函数本身的计算非常简单，那么构建计算图本身的时间就会相对非常浪费；
+    2.当我们需要在函数之中定义 tf.Variable 的时候，因为 tf.function 可能会被调用多次，因此定义 tf.Variable 会产生重复定义的情况。
+
+    """
+    a = tf.constant([[10, 10], [11., 1.]])
+    x = tf.constant([[1., 0.], [0., 1.]])
+    b = tf.Variable(12.)
+    y = tf.matmul(a, x) + b
+    print("PRINT: ", y)
+    tf.print("TF-PRINT: ", y)
+    return y
+
+
+class Func02:
+    def __init__(self):
+        self._b = None
+
+    @tf.function
+    def __call__(self):
+        a = tf.constant([[10, 10], [11., 1.]])
+        x = tf.constant([[1., 0.], [0., 1.]])
+        if self._b is None:
+            self._b = tf.Variable(12.)  # 保证tf.Variable变量只被创建一次
+        y = tf.matmul(a, x) + self._b
+        print("PRINT: ", y)
+        tf.print("TF-PRINT: ", y)
+        return y
+
+
+def func03(a, b):
+    print('this runs at trace time; a is', a, 'and b is', b)
+    return b
+
+
+def func_test01():
+    print(func03(1, tf.constant(1)))
+    print("-" * 36)
+    print(func03(1, tf.constant(2)))
+    print("-" * 36)
+    print(func03(2, tf.constant(1)))
+    print("-" * 36)
+    print(func03(2, tf.constant(2)))
+
+
+@tf.function
+def func04(x):
+    return tf.abs(x)
+
+
+def func_test02():
+    f1 = func04.get_concrete_function(1)
+    f2 = func04.get_concrete_function(2)
+    print(f1 is f2)
+
+
 def main():
-    tape_sgd_test03b()
+    func_test02()
+    # func02 = Func02()
+    # func02()
 
 
 if __name__ == '__main__':
