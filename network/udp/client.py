f26a244d0140b2f9b72568790867d3bdaae59db8
caoxiaoqiang <checao@tencent.com>
2022-11-16 10:19:20 +0800

    udp communication

--- network/udp/client.py
+++ network/udp/client.py
@@ -2,17 +2,46 @@
 import socket
 import time
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-PORT = 9090
-server_address = ("localhost", PORT)
 
-while True:
-    print("=" * 36)
-    start = time.time()
-    print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start)))
-    msg = input("content:")
-    client_socket.sendto(msg.encode(), server_address)
-    now = time.time()
-    run_time = now - start
-    print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(now)))
-    print(f"run_time: {run_time} seconds\n")
+def debug00():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    PORT = 9090
+    server_address = ("localhost", PORT)
+
+    while True:
+        print("=" * 36)
+        start = time.time()
+        print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start)))
+        msg = input("content:")
+        client_socket.sendto(msg.encode(), server_address)
+        now = time.time()
+        run_time = now - start
+        print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(now)))
+        print(f"run_time: {run_time} seconds\n")
+
+
+def debug01():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_address = ("localhost", 9091)
+    client_socket.bind(client_address)
+    client_socket.settimeout(10)
+
+    server_address = ("localhost", 9090)
+
+    while True:
+        print("=" * 36)
+        start = time.time()
+        print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start)))
+        msg = input("content:")
+        client_socket.sendto(msg.encode(), server_address)
+        now = time.time()
+        run_time = now - start
+        print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(now)))
+        print(f"run_time: {run_time} seconds\n")
+        print("-" * 36)
+        receive_data, server = client_socket.recvfrom(1024)
+        print(f"from:{server},content:{receive_data.decode()}\n")
+
+
+if __name__ == '__main__':
+    debug01()
--- network/udp/server.py
+++ network/udp/server.py
@@ -3,18 +3,45 @@
 import socket
 import time
 
-PORT = 9090
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-address = ("0.0.0.0", PORT)
-server_socket.bind(address)
-server_socket.settimeout(10)
 
-while True:
-    try:
-        now = time.time()
-        print("=" * 36)
-        receive_data, client = server_socket.recvfrom(1024)
-        print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(now)))
-        print(f"from:{client},content:{receive_data.decode()}\n")
-    except socket.timeout:
-        print("timeout\n")
+def debug00():
+    PORT = 9090
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    address = ("0.0.0.0", PORT)
+    server_socket.bind(address)
+    server_socket.settimeout(10)
+
+    while True:
+        try:
+            now = time.time()
+            print("=" * 36)
+            receive_data, client = server_socket.recvfrom(1024)
+            print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(now)))
+            print(f"from:{client},content:{receive_data.decode()}\n")
+        except socket.timeout:
+            print("timeout\n")
+
+
+def debug01():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_address = ("0.0.0.0", 9090)
+    server_socket.bind(server_address)
+    server_socket.settimeout(10)
+
+    client_address = ("localhost", 9091)
+
+    while True:
+        try:
+            now = time.time()
+            print("=" * 36)
+            receive_data, client = server_socket.recvfrom(1024)
+            print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(now)))
+            print(f"from:{client},content:{receive_data.decode()}\n")
+            reply = "got"
+            server_socket.sendto(reply.encode(), client_address)
+        except socket.timeout:
+            print("receiving ......")
+
+
+if __name__ == '__main__':
+    debug01()
