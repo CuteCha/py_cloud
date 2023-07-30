e42f67ce01eb0052c3fac89548d96dd4d47f46f4
cxq <chmath@126.com>
2023-07-30 17:17:24 +0800

    args

--- arguments/args_format.py
+++ arguments/args_format.py
@@ -7,10 +7,12 @@ def main():
     ap.add_argument("-m", "--model", default="logs/h5/fashionnet.h5", help="path to output h5 model")
     ap.add_argument("-p", "--pb-model", default="logs/pb", help="path to output pb model")
     ap.add_argument("-g", "--h5_model", default="logs/pb", help="path to output pb model")
+    ap.add_argument("--ab-cl-df", default="ab-cl-df-d", help="path to output pb model", dest="aab_cl_df")
     ap.add_argument("-c", "--color", default="logs/label_map/color.pickle", help="path to output color label binarizer")
     ap.add_argument("-f", "--fig", default="logs/fig", help="base filename for generated plots")
-    ap.add_argument("-l", "--cate", default="logs/label_map/category.pickle",
-                    help="path to output category label binarizer")
+    ap.add_argument("-l", "--cate", default="logs/label_map/category.pickle", help="path to output category label")
+    ap.add_argument('--foobar', action='store_true')
+    ap.add_argument('--foonley', action='store_false')
 
     group = ap.add_argument_group("ckpt", "conf")
     group.add_argument("--ckpt_dir", default="logs/ckpt", help="path to output ckpt")
@@ -22,6 +24,8 @@ def main():
     print(args_obj.ckpt_dir)
     ap.attr = 0
     print(ap.attr)
+    print(args_obj.aab_cl_df)
+    ap.parse_args(['--foob'])
 
 
 def debug_assert(a, b):
